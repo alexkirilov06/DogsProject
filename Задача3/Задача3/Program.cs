90a03349726771501a70ba200e26b2418d12bba0
alexkirilov06 <alexkrisi1996@gmail.com>
2024-02-12 12:19:37 +0200

    Upload DogsProject

---
+++ Задача3/Задача3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Задача3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            Console.WriteLine(101);
+            Console.WriteLine(1001);
+        }
+    }
+}
